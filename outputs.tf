e8ba9344efa955e0bd4b136ad918cf7a1509d572
Pallavi C <info@joindevops.com>
2025-02-06 21:55:04 +0530

    vpc-module-test

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,22 @@
 # output "subnets_info" {
 #    value = aws_subnet.public
 # }
+
+output "vpc_id" {
+  value = aws_vpc_main.id
+}
+
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+  
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+  
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+  
+}
